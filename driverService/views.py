e765f97d133c13b6ba2380db8c270a2a3d17acdc
Saion1992 <cyan241992@gmail.com>
2023-12-20 18:38:57 +0530

    Changes

--- driverService/views.py
+++ driverService/views.py
@@ -4,7 +4,7 @@ from venv import logger
 import jwt
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
-from jose import jwt, JWTError
+from jose import JWTError
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
 from rest_framework.decorators import api_view
