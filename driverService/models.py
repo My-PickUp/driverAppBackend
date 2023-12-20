5df455d8b0fc9997a96496379d08cd02b039736d
Saion1992 <cyan241992@gmail.com>
2023-12-20 20:19:12 +0530

    driverService/models.py

--- driverService/models.py
+++ driverService/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Driver(models.Model):
     driver_id = models.AutoField(primary_key = True)
     driver_status = models.CharField(max_length = 12, default='default_value_here')
+    current_lat = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    current_lng = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
     assigned_users = models.ManyToManyField('UserInfo', blank=True)
     name = models.CharField(max_length=255)
     phone = models.CharField(max_length=12)
