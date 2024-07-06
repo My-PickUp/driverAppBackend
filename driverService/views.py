141748a4a5682effbb4efcd1a0113a43d6530eee
Saion1992 <cyan241992@gmail.com>
2024-07-06 10:14:07 +0530

    Incorporated proper API responses and validation checks in form_upload api

--- driverService/views.py
+++ driverService/views.py
@@ -298,15 +298,22 @@ related informations into the tables driverride, customer and co_passengers.
 @ratelimit(key='ip', rate='1/s', block=True)
 @api_view(['POST'])
 def form_upload_response(request):
+
+    required_headers = {'driver', 'co_passenger', 'ride_type', 'drop_priority'}
+
     if 'csv_file' not in request.FILES:
         return Response({"error": "CSV file is missing"}, status=status.HTTP_400_BAD_REQUEST)
 
     csv_file = request.FILES['csv_file'].read().decode('utf-8').splitlines()
     reader = csv.DictReader(csv_file)
 
+    if not required_headers.issubset(set(reader.fieldnames)):
+        return Response({"error": f"CSV headers must include {', '.join(required_headers)}"}, status=status.HTTP_400_BAD_REQUEST)
+
     ride_details = []
 
     for row in reader:
+
         ride_date_time = row.get('ride_date_time', '')
         driver_id = row.get('driver', '')
         ride_type = row.get('ride_type', '').strip().capitalize()
@@ -323,6 +330,10 @@ def form_upload_response(request):
         customer_lon_pickup = row.get('customer_lon_pickup', '')
         customer_lat_drop = row.get('customer_lat_drop', '')
         customer_lon_drop = row.get('customer_lon_drop', '')
+
+        if not driver_id or not ride_type or not drop_priority:
+            return Response({"error": f"Invalid data in row: {row}"}, status=status.HTTP_400_BAD_REQUEST)
+
 
         ride_details.append({
             "ride_date_time": ride_date_time,
